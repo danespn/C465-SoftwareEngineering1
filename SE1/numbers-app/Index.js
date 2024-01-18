6c6f1b3f2e55e880aef985398f871b3b4a76a77b
Spencer, Dane Sullivan <danespen@iu.edu>
2024-01-18 16:07:29 -0500

    add and change index.js

---
+++ SE1/numbers-app/Index.js
@@ -0,0 +1,9 @@
+const express = required('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic service - Hello World!');
+});
+
+app.listen(port);
